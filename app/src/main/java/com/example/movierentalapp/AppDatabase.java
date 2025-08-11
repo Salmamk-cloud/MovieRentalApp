33468844255881fa1fbd541cfd86f58f1f4453a5
Salmamk-cloud <mekdadsalma33@gmail.com>
2025-08-10 23:07:36 -0400

    Mettre en place le singleton Room AppDatabase

--- app/src/main/java/com/example/movierentalapp/AppDatabase.java
+++ app/src/main/java/com/example/movierentalapp/AppDatabase.java
@@ -1,1 +1,21 @@
+@Database(entities = {User.class, Movie.class, Rental.class}, version = 2, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract UserDao userDao();
+    public abstract MovieDao movieDao();
+    public abstract RentalDao rentalDao();
+
+    private static volatile AppDatabase INSTANCE;
+    public static AppDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.getApplicationContext(), AppDatabase.class, "movie_database"
+                    ).fallbackToDestructiveMigration().build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
 
