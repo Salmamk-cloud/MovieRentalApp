86c34317a0692c7ad5c5b5c46c5290ef30a65278
Nouha123221 <bout.2004.nouhaila@gmail.com>
2025-08-11 05:03:16 +0200

    Vérifier/déclarer les dépendances Gradle (Room + Picasso)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,4 @@
-
+val roomVersion = "2.6.1"
+implementation("androidx.room:room-runtime:$roomVersion")
+annotationProcessor("androidx.room:room-compiler:$roomVersion")
+implementation("com.squareup.picasso:picasso:2.8")
